a49a13d894e947bcf18b2b97faf6c90ac9c78545
Horacio Abitu <ahabitu@gmail.com>
2024-05-25 12:07:10 -0300

    Second commit

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -8,13 +8,13 @@ export default function Inputypes() {
     <div className='flex flex-col justify-center items-center mt-5 gap-4'>
         Input Types
         <label for="name">Name: Type Text</label>
-        <input type="text" id="name" name="name"/>
+        <input type="text" id="name" name="name" className='text-black'/>
 
         <label for="quantity">Quantity: Type Number</label>
-        <input type="number" id="quantity" name="quantity" min="1" max="10000"/>
+        <input type="number" id="quantity" name="quantity" min="1" max="10000" />
 
         <label for="quantity">Type Number: Input Mode numeric</label>
-        <input type="number" inputMode='numeric' id="quantity" name="quantity" min="1" max="10000"/>
+        <input type="number" inputMode='numeric' id="quanti" name="quanti" min="1" max="10000" className='text-black'/>
     </div>
   )
 }
